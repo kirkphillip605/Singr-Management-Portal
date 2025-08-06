60d7109a45e81173c69896423e570b0f94553b63
Phillip Kirk <168415485+kirkphillip605@users.noreply.github.com>
2025-08-06 09:30:12 -0500

    Fix cookies called outside request scope error

--- src/lib/auth-server.ts
+++ src/lib/auth-server.ts
@@ -1,10 +1,6 @@
 import { getServerSession } from 'next-auth/next'
-import { headers, cookies } from 'next/headers'
 import { authOptions } from '@/lib/auth'
 
 export async function getAuthSession() {
-  return await getServerSession(authOptions, { 
-    headers: headers(), 
-    cookies: cookies() 
-  })
+  return await getServerSession(authOptions)
 }
