92c4259050ef87605c6a00569e5adfb8d9f26bb0
Phillip Kirk <168415485+kirkphillip605@users.noreply.github.com>
2025-10-08 01:05:37 -0500

    Use environment variable for Stripe API version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-07-30.basil',
+  apiVersion: process.env.STRIPE_API_VERSION,
   typescript: true,
 })
 
